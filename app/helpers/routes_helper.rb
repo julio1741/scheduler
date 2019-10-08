67398734c285a414e96832f5d821eaf855a7ee92
Julio Baptista <julio1741@gmail.com>
2019-10-08 15:11:45 -0400

    Add scopes for Route model and resolve_routes method helper

--- app/helpers/routes_helper.rb
+++ app/helpers/routes_helper.rb
@@ -1,2 +1,32 @@
 module RoutesHelper
+
+  def resolve_routes
+    unresolved_routes = Route.not_resolved
+
+    unresolved_routes.each do |ur|
+
+      range = [ur.starts_at, ur.ends_at]
+
+      available_vehicles = get_available_vehicles(range)
+      available_drivers = get_available_drivers(range)
+
+      next if available_vehicles.nil? or available_drivers.nil?
+      vehicle = available_vehicles.first
+      driver = available_drivers.first
+
+      ur.driver_id = driver.id
+      ur.vehicle_id = vehicle.id
+      ur.resolved = true
+      ur.save
+    end
+
+  end
+
+  def get_available_vehicles range
+     Vehicle.where.not(id:Route.assigned_range(range).pluck(:vehicle_id))
+  end
+
+  def get_available_drivers range
+    Driver.where.not(id:Route.assigned_range(range).pluck(:driver_id))
+  end
 end
--- app/models/route.rb
+++ app/models/route.rb
@@ -1,4 +1,25 @@
 class Route < ApplicationRecord
-  belongs_to :user_id
-  scope :not_resolved_routes, where(resolved: false)
+  #belongs_to :user_id
+
+  #Rutas no asignadas aun
+  scope :not_resolved, -> { where(resolved: false) }
+
+  #Rutas asignadas
+  scope :resolved, -> { where(resolved: true) }
+
+  #Vehiculos no ocupados
+  scope :available_vehicles, -> { where(vehicle_id: nil) }
+
+  #Conductor no ocupado
+  scope :available_drivers, -> { where(driver_id: nil) }
+
+  #Verifica si esta asignado un rango de fecha a alguna ruta
+  scope :assigned_range, -> (needed_range) do
+    resolved.where("(? BETWEEN routes.starts_at AND routes.ends_at
+            OR ? BETWEEN routes.starts_at AND routes.ends_at)
+            OR routes.starts_at BETWEEN ? AND ?
+            OR routes.ends_at BETWEEN ? AND ?
+            ", needed_range.first, needed_range.last, needed_range.first, needed_range.last, needed_range.first, needed_range.last)
+  end
+
 end
